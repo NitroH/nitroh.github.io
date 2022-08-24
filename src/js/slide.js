88f8236586f21018dfc864dec7e4ebafb3a8c053
NitroH <nitro.hyt@gmail.com>
2022-08-24 23:16:36 +0200

    last modification

--- src/js/slide.js
+++ src/js/slide.js
@@ -1,6 +1,13 @@
-const Element_change = document.querySelector('#ScrollEvent')
+const Element_change = document.querySelector('#ScrollEvent');
 
-window.addEventListener('scroll', () => {
-    const scroll_value = window.scrollY
-    Element_change.style.marginLeft = `${scroll_value - 500}px`;
-});
+    window.addEventListener('scroll', () => {
+        const scroll_value = window.scrollY;
+        Element_change.style.marginLeft = `${scroll_value - 500}px`;
+
+        if(scroll_value >= 1400) {
+            const scroll_value = 0;
+            Element_change.style.marginLeft = `${scroll_value - 500}px`;
+        } else{
+            Element_change.innerHTML = "ABOUT ME";
+        }
+    });
--- src/js/table.js
+++ src/js/table.js
@@ -6,11 +6,13 @@ const job = document.querySelector('.nav-journey :nth-child(6)');
 const text = document.querySelector('.txt-journey');
 const line = document.querySelector('.line2');
 const title = document.querySelector('.title-journey');
-const date = document.querySelector('.date');
+const date = document.querySelector('.date-journey');
 
 
 formation.onclick = function() {
-    text.innerHTML = "> For the moment, I didn't do any professional formations because I always find what I need in Internet. But, I listen podcast , I read programming book and I also do online lessons.";
+    title.innerHTML = "Training";
+    date.innerHTML = "All time -";
+    text.innerHTML = "> For the moment, I didn't do any professional formations because I'm too young. But, I listen podcast , I read programming book and I also do online lessons.";
     document.querySelector('.line2').style.marginTop = "0px";
     formation.style.fontWeight = "500";
     formation.style.color = "#383838";
@@ -18,27 +20,35 @@ formation.onclick = function() {
 
 middle.onclick = function() {
     text.innerHTML = "> Actually in middle school. I learn a lot about HTML, CSS, JavaScript, React, Node JS";
+    title.innerHTML = "Starting";
+    date.innerHTML = "September 2020 -";
     document.querySelector('.line2').style.marginTop = "70px";
     middle.style.fontWeight = "500";
     middle.style.color = "#383838";
 };
 
 college.onclick = function() {
-    text.innerHTML = "> I'm not in high school yet, but i've some objectives when I will be there <br> > Learn more programming language, upgrade my english, have a big project like create an application...";
+    title.innerHTML = "High School";
+    date.innerHTML = "September 2023 -";
+    text.innerHTML = "> I'm not in high school yet, but i want to learn a lot about machine learning, python, and upgrade my english level !";
     document.querySelector('.line2').style.marginTop = "140px";
     college.style.fontWeight = "500";
     college.style.color = "#383838";
 };
 
 studies.onclick = function() {
-    text.innerHTML = "> As I write this text I'm just in middle school, but I'm very pretensious about my future. <br> > I would like to work in our future life on this world surrounded by technologies";
+    title.innerHTML = "Studies";
+    date.innerHTML = "Somewhere in time";
+    text.innerHTML = "> As I write this text I'm just in middle school, but I'm very pretentious about my future and I would like to work in our future life with technologies. <br> <br> > I would like to do a <i>PREPA mp2i</i>which is a special class where you work a lot about computer sciences and maths.";
     document.querySelector('.line2').style.marginTop = "210px";
     studies.style.fontWeight = "500";
     studies.style.color = "#383838";
 };
 
 job.onclick = function() {
-    text.innerHTML = "> As I write this text I'm just in middle school, but I'm very pretensious about my future. <br> > As you may have understood, my dream is to work in Google, I really like to work there because it's the place of the future inovations.";
+    title.innerHTML = "Studies";
+    date.innerHTML = "Somewhere in time";
+    text.innerHTML = "> As I write this text I am just in college, but I am very pretentious about my future. <br> <br> > You will understand, my dream is to work at Google, I would very much like to work there because it is the place of future innovations.";
     document.querySelector('.line2').style.marginTop = "280px";
     job.style.fontWeight = "500";
     job.style.color = "#383838";
